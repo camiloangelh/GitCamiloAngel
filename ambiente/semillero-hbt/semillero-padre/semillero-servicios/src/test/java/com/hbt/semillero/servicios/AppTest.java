1c426ee19e0d0d0cdd588da2d77ca1208ad867fd
cangelh <cangelh@uqvirtual.edu.co>
2019-10-28 23:21:56 -0500

    Se modifico el archivo AppTest añadiendo el metodo para validar cadenaInvertida

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,61 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
 	}
-}
+	
+	/**
+	 * Metodo para invertir cadena
+	 * 
+	 * @param cadena
+	 * @return cadena invertida
+	 */
+	private String invertirCadena(String cadena) {
+
+		String cadenaInvertida = "";
+
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+
+		return cadenaInvertida;
+	}
+
+	@Test
+	public void primeraPrueba() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L;
+		Long sumando2 = 50L;
+		Long resultado = sumando1 + sumando2;
+
+		Assert.assertEquals(resultado, resultadoEsperado);
+		resultadoEsperado = 200L;
+		Assert.assertNotEquals(resultado, resultadoEsperado);
+
+	}
+
+	/**
+	 * Valida el metodo invertirCadena()
+	 */
+	@Test
+	public void validarInvertirCadena() {
+		String cadena = "prueba";
+		String cadenaEsperada = "abeurp";
+		String cadenaInvertida = invertirCadena(cadena);
+		
+		Assert.assertEquals(cadenaEsperada, cadenaInvertida);
+		
+		Assert.assertNotEquals(cadena, cadenaEsperada);
+		
+//		Debe generar error
+//		Assert.assertEquals(cadena, cadenaInvertida);
+		
+	}
+
+}// Cierre de la clase
